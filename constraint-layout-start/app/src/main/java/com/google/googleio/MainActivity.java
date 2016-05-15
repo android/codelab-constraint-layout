038aa603121452068b035c0a8758c2a493445c8c
Nagesh Susarla <nageshs@google.com>
2016-05-14 17:22:28 -0700

    Add layouts for steps into project
    Instead of asking developers to use different
    projects/directories for each step, simplify
    the process by using different layouts for each
    step.

--- constraint-layout-start/app/src/main/java/com/google/googleio/MainActivity.java
+++ constraint-layout-start/app/src/main/java/com/google/googleio/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_start);
     }
 }
